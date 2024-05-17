1bf26bafd14020ccb5a67f49d0a181b02657a2fa
andres9735 <ivanandressch@gmail.com>
2024-05-17 18:11:15 -0300

    Primer Commit

---
+++ reloj.js
@@ -0,0 +1,20 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const monthNames = ["Enero", "Febrero", "Marzo",
+                      "Abril", "Mayo", "Junio", 
+                      "Agosto", "Septiembre", "Octubre",
+                      "Noviembre", "Diciembre",];
+
+const interval = setInterval(() => {
+
+    const local = new Date();
+
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthNames[month]} ${year}`;
+
+}, 1000);
